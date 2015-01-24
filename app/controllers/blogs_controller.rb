3bf9a86292a9b33606b893306efb04e67c1db7b9
Max Jacobson <max@hardscrabble.net>
2015-01-24 13:51:53 -0500

    Add a root path

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -65,6 +65,10 @@ class BlogsController < ApplicationController
     end
   end
 
+  def root_redirect
+    redirect_to root_path
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
